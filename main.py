3a4939cbbf68c689f572d5248a1f7d85b0686a18
Mike <56518407+redmictian@users.noreply.github.com>
2021-06-12 16:31:27 +0000

    Prepared it to be the lab app

--- main.py
+++ main.py
@@ -1,1 +1,7 @@
-print('h')
+from random import randrange
+test_string = ['a','b','c','d','e']
+random_string = ''
+for i in test_string:
+  counter = randrange(len(test_string))
+  random_string += test_string[counter]
+print(random_string)
